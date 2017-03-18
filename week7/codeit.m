08c4163f6e1e154ffc9d26e859ffe8215fad5eed
Jie <1007172767@qq.com>
2017-03-18 23:31:15 +0800

    'week7'

--- week7/codeit.m
+++ week7/codeit.m
@@ -1,16 +1,11 @@
-function coded = codeit(txt)
-%Input txt is a string
-%Output coded is a string
-%Example coded=codeit('azAZ')
-code=double(txt);
+function coded = codeit(str)
+code=double(str);
 s=length(code);
 for i=1:s
-    if code(i)>=65 && code(i)<=90 %for uppercase  letters
-        m=code(i)-65;
-        code(i)=90-m;
-    elseif code(i)>=97 && code(i)<=122 %for lowercase  letters
-        m=code(i)-97;
-        code(i)=122-m;
+    if code(i)>=65 && code(i)<=90
+        code(i) = 65+90-code(i);
+    elseif code(i)>=97 && code(i)<=122
+        code(i) = 97+122-code(i);
     end
 end
 coded=char(code);
